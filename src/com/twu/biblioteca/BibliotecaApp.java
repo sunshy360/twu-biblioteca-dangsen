49671bb7991ae5c8f97dae982589410562807e0a
sunshy360 <sunshy_129@sina.com>
2017-08-06 18:43:57 +0800

    getAllBookList and getBookDetail Functions

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,31 @@
 package com.twu.biblioteca;
 
 public class BibliotecaApp {
+    private String[] bookList= {"Math","Chemical","Computer"};
+    private String[] authorList= {"John","Mike","Lucy"};
+    private String[] publishList= {"A","B","C"};
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+    }
+
+
+    public String getAllBooksList() {
+        String bookListStr = "";
+        for(String book:bookList){
+            bookListStr += book + "\n";
+        }
+        return bookListStr;
+    }
+
+    public String getBookDetail(String bookName) {
+        String bookDetailStr = "";
+        for(int i=0;i<bookList.length;i++){
+            if(bookList[i].equals(bookName)){
+                bookDetailStr = bookName + "\n" +authorList[i] + "\n" + publishList[i] + "\n";
+                break;
+            }
+        }
+        return bookDetailStr;
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -5,6 +5,20 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
+    public BibliotecaApp bibliotecaAppObj = new BibliotecaApp();
+
+    @Test
+    public void getAllBooksListTest() {
+        assertEquals("Math\nChemical\nComputer\n", bibliotecaAppObj.getAllBooksList());
+    }
+
+
+    @Test
+    public void getBookDetailTest() {
+        assertEquals("Math\nJohn\nA\n", bibliotecaAppObj.getBookDetail("Math"));
+        assertEquals("Chemical\nMike\nB\n", bibliotecaAppObj.getBookDetail("Chemical"));
+    }
+
 
     @Test
     public void test() {
